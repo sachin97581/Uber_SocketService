71edbe30a269c5c1d6afa67eb7c7061f677bee08
sachin97581 <sachin97581@gmail.com>
2025-09-03 18:18:42 +0530

    make change in UberSockectSerciceApplication file

--- src/main/java/com/example/UberSocketService/UberSocketServiceApplication.java
+++ src/main/java/com/example/UberSocketService/UberSocketServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.UberSocketService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.example.uberProject_EntityService.model")
 public class UberSocketServiceApplication {
 
 	public static void main(String[] args) {
